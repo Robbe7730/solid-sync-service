59060ab9ab0f8bad907d6fa99a6c2e2f76c72266
Robbe Van Herck <robbe@robbevanherck.be>
2022-05-17 20:07:20 +0200

    Put it in docker for semantic.works

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
+import mu from 'mu';
+
 import { getPod } from './solid.js';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const podBase = process.env.POD_BASE || "http://localhot:3000";
+checkEnv();
 
-const podName = process.env.POD_NAME || "robbevanherck";
+mu.app.post('/update', (_req, res) => {
+  update().then(() => {
+    res.send("OK");
+  });
+});
 
-let podPath = podBase;
+async function update() {
+  const podBase = process.env.POD_BASE || "http://localhost:3000";
 
-if (podPath[podPath.length-1] !== '/') {
-  podPath = podPath + "/" + podName + "/";
-} else {
-  podPath = podPath + podName + "/";
+  const podName = process.env.POD_NAME || "robbevanherck";
+
+  let podPath = podBase;
+
+  if (podPath[podPath.length-1] !== '/') {
+    podPath = podPath + "/" + podName + "/";
+  } else {
+    podPath = podPath + podName + "/";
+  }
+
+  const podStore = await getPod(podPath);
+  console.log(podStore);
 }
 
-const podStore = await getPod(podBase + "/" + podName + "/");
-console.log(podStore);
+function checkEnv() {
+  if (!process.env.POD_BASE) {
+    throw new Error("No POD_BASE specified");
+  }
+  if (!process.env.POD_NAME) {
+    throw new Error("No POD_NAME specified");
+  }
+  if (!process.env.TOKEN_ID) {
+    throw new Error("No TOKEN_ID specified");
+  }
+  if (!process.env.TOKEN_SECRET) {
+    throw new Error("No TOKEN_SECRET specified");
+  }
+}
